a18acc1ddbac265c849d80e9d00b092041d080b2
cheesecup <couplejwsa@gmail.com>
2022-10-08 01:01:57 +0900

    #9 - 스프링 시큐리티 Config 업데이트 및 `UserDetailService` 구현
    * 로그인과 로그아웃을 위한`SecurityConfig` 의 configure(HttpSecurity) 메소드 업데이트
    * configure(auth) 메서드 추가
    * `MemberService`에 `UserDetailsService` 인터페이스 구현체 작성

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -2,17 +2,43 @@ package com.shop.service;
 
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+
+    /* UserDetailsService 인터페이스 구현체
+     * DB 에서 회원정보를 가지고 오는 역할을 한다.
+     * 회원 정보를 조회하여 사용자의 정보와 권한을 갖는 UserDetails 인터페이스를 반환한다.
+     * Spring Security 에서 제공하는 User.class 는 UserDetails 를 구현하는 클래스이다.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        UserDetails user = User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+
+        return user;
     }
 
     /* 회원 저장 */
