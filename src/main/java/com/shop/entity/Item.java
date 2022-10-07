8f5f0aa83bdbd8c233c2d759a0c8778b503e08e2
cheesecup <couplejwsa@gmail.com>
2022-10-07 22:00:34 +0900

    #6 - querydsl를 사용하여 검색 기능 테스트
    querydsl 문법을 이용하여 상품 이름 검색 코드 테스트

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -66,7 +66,7 @@ public class Item {
         if (this == o) return true;
         if (!(o instanceof Item)) return false;
         Item item = (Item) o;
-        return Objects.equals(id, item.id);
+        return id != null && id.equals(item.id);
     }
 
     @Override
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,13 +1,24 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +28,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 //@TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
 
+    @PersistenceContext
+    EntityManager em;
     private final ItemRepository itemRepository;
 
     ItemRepositoryTest(@Autowired ItemRepository itemRepository) {
@@ -60,5 +73,44 @@ class ItemRepositoryTest {
         itemRepository.delete(findItem);
 
         assertThat(itemRepository.count()).isEqualTo(previousCount - 1);
+    }
+
+    @Test
+    @DisplayName("querydsl 을 이용하여 상품 이름 검색 테스트")
+    void useQuerydsl_searchItemTest() {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+
+        List<Item> pencilItemList = query.select(qItem)
+                .from(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemName.like("%연필%"))
+                .orderBy(qItem.price.asc())
+                .fetch();
+
+        assertThat(pencilItemList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("querydsl 을 사용하여 상품 검색 조회 테스트2")
+    void useQuerydsl_searchItemTest2() {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem qItem = QItem.item;
+
+        String name = "연필";
+        int price = 1000;
+        String status = "SELL";
+
+        BooleanBuilder itemName = booleanBuilder.and(qItem.itemName.like("%" + name + "%"));
+        BooleanBuilder itemPrice = booleanBuilder.and(qItem.price.loe(price));
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+
+        List<Item> itemList = itemPagingResult.getContent();
+        for (Item item : itemList) {
+            System.out.println("검색된 상품 이름 : " + item.getItemName());
+        }
+
     }
 }
