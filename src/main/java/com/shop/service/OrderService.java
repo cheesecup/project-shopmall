69b0b1d0f6388c18320608f04999f9b6a00cf880
cheesecup <couplejwsa@gmail.com>
2022-10-28 19:12:05 +0900

    #25 - 주문 이력 조회 컨트롤러와 서비스 로직 작성
    `email` 과 `Pageable` 를 통해 이력 조회 후 Page<> 형태로 반환

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.dto.OrderHisDto;
+import com.shop.dto.OrderItemDto;
+import com.shop.entity.*;
+import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,13 +27,16 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public OrderService(@Autowired OrderRepository orderRepository,
                         @Autowired ItemRepository itemRepository,
-                        @Autowired MemberRepository memberRepository) {
+                        @Autowired MemberRepository memberRepository,
+                        @Autowired ItemImgRepository itemImgRepository) {
         this.orderRepository = orderRepository;
         this.itemRepository = itemRepository;
         this.memberRepository = memberRepository;
+        this.itemImgRepository = itemImgRepository;
     }
 
     /* 주문 기능을 수행하는 로직 */
@@ -46,5 +52,29 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    /* 주문 목록을 조회하는 로직 */
+    @Transactional(readOnly = true)
+    public Page<OrderHisDto> getOrderList(String email, Pageable pageable) {
+        List<Order> orders = orderRepository.findOrders(email, pageable); //주문 목록 조회
+        Long totalCount = orderRepository.countOrder(email); //유저의 주문 총 개수
+
+        List<OrderHisDto> orderHisDtoList = new ArrayList<>();
+
+        for (Order order : orders) {
+            OrderHisDto orderHisDto = new OrderHisDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepImgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHisDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHisDtoList.add(orderHisDto);
+        }
+
+        return new PageImpl<OrderHisDto>(orderHisDtoList, pageable, totalCount);
     }
 }
